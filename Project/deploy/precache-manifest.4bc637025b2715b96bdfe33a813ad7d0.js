4e00b8ca7bc5f28b03e076dc0e407ee1f4deddcd
Pedro Tavares <ptcar@inspr.com>
2020-06-13 22:33:32 -0300

    feat: adds react front end

---
+++ Project/deploy/precache-manifest.4bc637025b2715b96bdfe33a813ad7d0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a307934f9da33c0bab760d52846fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b652e504769b2782a971",
+    "url": "/static/css/main.c9b4d355.chunk.css"
+  },
+  {
+    "revision": "7ca9ec3b0bda62826b61",
+    "url": "/static/js/2.c3a48dcb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c3a48dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b652e504769b2782a971",
+    "url": "/static/js/main.89844212.chunk.js"
+  },
+  {
+    "revision": "e06c96b9be6927f5c0c3",
+    "url": "/static/js/runtime-main.10bc7ff2.js"
+  },
+  {
+    "revision": "27bb46a471e7a86a82d3d5397365dabd",
+    "url": "/static/media/FiraCode-Regular.27bb46a4.otf"
+  },
+  {
+    "revision": "0a1189e59768e63b5993188f6853dd1e",
+    "url": "/static/media/Subtract.0a1189e5.svg"
+  },
+  {
+    "revision": "085915d996c35754b11f41e183ca7e72",
+    "url": "/static/media/bob_ross.085915d9.jpg"
+  },
+  {
+    "revision": "5df9effc5aa08a77177fcdb4beb063cd",
+    "url": "/static/media/gordon.5df9effc.jpg"
+  },
+  {
+    "revision": "fdb8eff0993d67efb7604a7ba33cbb44",
+    "url": "/static/media/jesus.fdb8eff0.svg"
+  }
+]);
